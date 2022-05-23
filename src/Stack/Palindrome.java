833d51f4365f4500dff1bf0e287dd9f180e9327a
dindanurhalisa <dindahalisa12@gmail.com>
2022-05-23 22:50:34 +0700

    update

--- src/Stack/Palindrome.java
+++ src/Stack/Palindrome.java
@@ -6,26 +6,22 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String inputString = "";
+        String inputKata = "";
         Stack<Character> stack = new Stack<Character>();
-
         do {
             System.out.print("Kata : ");
-            inputString = in.nextLine();
-            for (int i = 0; i < inputString.length(); i++) {
-                stack.push(inputString.charAt(i));
+            inputKata = in.nextLine();
+            for (int i = 0; i < inputKata.length(); i++) {
+                stack.push(inputKata.charAt(i));
             }
-
             StringBuilder reverseString = new StringBuilder();
-
             while (!stack.isEmpty()) {
                 reverseString.append(stack.pop());
             }
-
-            if (inputString.equals(reverseString.toString()))
-                System.out.println("The input String is a palindrome.");
+            if (inputKata.equals(reverseString.toString()))
+                System.out.println("Kata yang diinputkan adalah palindrome");
             else
-                System.out.println("The input String is not a palindrome.");
-        } while (!inputString.equals(""));
+                System.out.println("Kata yang diinputkan adalah bukan palindrome");
+        } while (!inputKata.equals(""));
     }
 }
